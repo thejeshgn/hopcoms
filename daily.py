6342875e51778e83c89c677052a27f7b1cecce65
Thejesh GN <i@thejeshgn.com>
2019-09-23 23:50:14 +0530

    Updated the scraper and item_list2 which is very different from item_list1

--- daily.py
+++ daily.py
@@ -17,7 +17,7 @@ all_item_list = {}
 hopcoms_meta 	= couch["hopcoms_meta"]
 
 if all_items_load:
-	with open('item_list.csv', "r") as csv_file:
+	with open('item_list2.csv', "r") as csv_file:
 		reader = csv.reader(csv_file)
 		header = True
 		for row in reader:
@@ -30,7 +30,7 @@ if all_items_load:
 			all_item_list[label]=int(row[0])
 
 	try:
-		if hopcoms_meta["item_codes"]:
+		if hopcoms_meta["item_codes2"]:
 			pass
 	except couchdb.http.ResourceNotFound:
 			print "add"
@@ -48,23 +48,22 @@ hopcoms_daily 	= couch["hopcoms_daily"]
 
 
 
-web_data_url = "http://www.hopcoms.kar.nic.in/(S(vks0rmawn5a2uf55i2gpl3zo))/RateList.aspx"
-table_x_path ="""//*[@id="ctl00_LC_grid1"]"""
+web_data_url = "http://hopcoms.karnataka.gov.in/CropRates.aspx"
 total_data = {}
 r = requests.get(web_data_url)
 if r.status_code == 200:
 	html = r.text
 	soup = BeautifulSoup(html)
-	date_span = soup.find(id='ctl00_LC_DateText')
+	date_span = soup.find(id='lblDate')
 	date_span_text = date_span.text
 	date_span_text = date_span_text.strip()
-	date_span_text = date_span_text.replace("Last Updated Date: ","")
+	#date_span_text = date_span_text.replace("Last Updated Date: ","")
 	date_span_text_array = date_span_text.split("/")
 	final_date = date_span_text_array[2]+date_span_text_array[1]+date_span_text_array[0]
 	print str("Updating for ="+final_date)
 
 
-	table = soup.find(id='ctl00_LC_grid1')
+	table = soup.find(id='grdRates')
 	#print str(table)
 	for tr in table:
 		if str(tr).strip() == "":
@@ -80,35 +79,21 @@ if r.status_code == 200:
 		data2=0
 
 		for th in tr.findChildren('td'):			
-			for span in th.findChildren('span'):
-				content = str(span.text).strip()
-
+			for span in th.findChildren('font'):
 				row = row + 1
-				if row == 1 or row == 4:
+				#print str(span)
+				if row == 3:
 					continue
-				if row == 2 and content != "":
+
+
+				content = str(span.text).strip()
+				if row == 1 :
 					label1 = str(content)				
-				if row == 3 and content != "":
-					data1 = float(content)				
 				
-				if row == 5 and content != "":
-					label2 = str(content)				
-				if row == 6 and content != "":
-					data2 = float(content)
-
-		if label1.strip() != "":
-			label1 = label1.replace(" ","")
-			label1 = label1.lower()
-			item_id = all_item_list[label1]
-			total_data[item_id]=data1				
-			
-
-		if label2.strip() != "":
-			label2 = label2.replace(" ","")
-			label2 = label2.lower()
-			item_id = all_item_list[label2]
-			total_data[item_id]=data2				
-
+				if row == 4:
+					data1 = float(content)
+					
+				total_data[label1]=data1								
 
 dt = datetime.date.today()
 _id = '{:%Y%m%d}'.format(dt)
