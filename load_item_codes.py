b31211c615eaebb3df9000534f5aa5adc7420fa3
Thejesh GN <i@thejeshgn.com>
2019-10-04 12:21:41 +0530

    updated the item details load script

---
+++ load_item_codes.py
@@ -0,0 +1,84 @@
+import requests
+import json
+import csv
+import datetime
+import couchdb
+from BeautifulSoup import BeautifulSoup
+
+#READ THESE FROM CONFIG
+config_file = "/home/thej/.config/code_config/hopcoms.json"
+config  = json.load(open(config_file))
+db_full_url= config["db_full_url"]
+couch = couchdb.Server(db_full_url)
+
+
+all_items_load = False
+hopcoms_meta 	= couch["hopcoms_meta"]
+
+#all_item_list = {}
+# with open('item_list.csv', "r") as csv_file:
+#     reader = csv.reader(csv_file)
+#     header = True
+#     for row in reader:
+#         if header:
+#             header = False
+#             continue
+#         label = (row[1]).strip()
+#         label =	label.replace(" ","")
+#         label = label.lower()
+#         all_item_list[label]=int(row[0])
+
+# try:
+#     if hopcoms_meta["item_codes"]:
+#         pass
+# except couchdb.http.ResourceNotFound:
+#         print "add"
+#         all_item_list["_id"]="item_codes"
+#         print str(all_item_list)
+#         hopcoms_meta.save(all_item_list)
+
+
+#all_item_list = {}
+# with open('item_list2.csv', "r") as csv_file:
+#     reader = csv.reader(csv_file)
+#     header = True
+#     for row in reader:
+#         if header:
+#             header = False
+#             continue
+#         label = (row[1]).strip()
+#         label =	label.replace(" ","")
+#         label = label.lower()
+#         all_item_list[label]=int(row[0])
+
+# try:
+#     if hopcoms_meta["item_codes2"]:
+#         pass
+# except couchdb.http.ResourceNotFound:
+#         print "add"
+#         all_item_list["_id"]="item_codes2"
+#         print str(all_item_list)
+#         hopcoms_meta.save(all_item_list)
+
+
+item_details_all = {}
+with open('item_details2.csv', "r") as csv_file:
+    reader = csv.reader(csv_file)
+    header = True
+    for row in reader:
+        if header:
+            header = False
+            continue
+        insertRow = {}
+        insertRow["name_en"] = row[1]
+        insertRow["name_kn"] = (row[2]).decode('utf-8')
+        item_details_all[row[0]] = insertRow
+
+try:
+    if hopcoms_meta["item_details2"]:
+        pass
+except couchdb.http.ResourceNotFound:
+        print "add"
+        item_details_all["_id"]="item_details2"
+        print str(item_details_all)
+        hopcoms_meta.save(item_details_all)
